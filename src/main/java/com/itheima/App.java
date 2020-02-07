dd9da910c7725caed3d218325caf604a75ef4657
yk <1461145531@qq.com>
2020-02-07 16:47:01 +0800

    增加输出

--- src/main/java/com/itheima/App.java
+++ src/main/java/com/itheima/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Hello Git!")
+        System.out.println("Hello Git!");
+        System.out.println("Hello Github!");
     }
 }
